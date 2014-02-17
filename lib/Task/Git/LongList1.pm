4f5919ab88410b3506239cc02bb3c906e403ab0b
Kevin William Henwood <ski@cpan.org>
2014-02-17 20:13:28 +0000

    Import of SKI/Task-Git-LongList1-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Git-LongList1
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SKI/Task-Git-LongList1-0.01.tar.gz
    gitpan-cpan-author:       SKI
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Git/LongList1.pm
@@ -0,0 +1,41 @@
+package Task::LongList1;
+
+use 5.005;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+
+=head1 NAME
+
+Task::Git::LongList1 -
+  Perl extension for installing git modules
+
+=head1 SYNOPSIS
+
+  perl -MCPAN -e 'install Task::Git::LongList1'
+
+=head1 DESCRIPTION
+
+Task::Git::LongList1 is the first part of three parts.
+  It's based on the long list as given by 01Modules.index
+
+=head1 AUTHOR
+
+Kevin W. Henwood, <Khen1950fx@yahoo.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014 by Kevin W. Henwood
+
+This library is free software; you can redistribute it 
+and/or modify it under the same terms as Perl itself, 
+either Perl version 5.005, or at your option, any later 
+version of Perl 5 you may have available.
+
+=cut
---
+++ t/Task-Git-LongList1.t
@@ -0,0 +1,12 @@
+#########################
+
+use strict;
+use warnings;
+
+use Test;
+BEGIN { plan tests => 1 };
+use Task::Git::LongList1;
+ok(1); 
+
+#########################
+
